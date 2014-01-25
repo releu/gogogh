270cb3a2ede3af98afa9de52e132aec1edf3111c
Jan Bernacki <releu@me.com>
2014-01-25 18:00:22 +0400

    fix js

--- public/game.js
+++ public/game.js
@@ -15,22 +15,69 @@ $(document).ready(function(){
     console.log(e)
   }
 
-  var Game = function(){
-    this.start = function(){
+  var Game = function(player1, player2){
+    var self = this
+    
+    self.player1 = player1
+    self.player2 = player2
+    
+    self.start = function(){
       var startNum = 3
       $(".screen").removeClass("active")
-      $(".starting").addClass("active")
+      
       var i = setInterval(function(){
+        $(".starting").addClass("active")
         $(".starting").text(startNum)
         startNum = startNum - 1
-        if (startNum == 0) {
+        if (startNum == -1) {
           clearInterval(i)
+          self.run()
         }
-      }, 1000)
+      }, 200)
+    }
+    
+    self.run = function(){
+      $(".screen").removeClass("active")
+      $(".race").addClass("active")
+      
+      var i1 = setInterval(function(){
+        if (self.player1.doStep()){
+          clearInterval(i1)
+          clearInterval(i2)
+        }
+      }, Math.floor((Math.random()*100)+100))
+      var i2 = setInterval(function(){
+        if (self.player2.doStep()){
+          clearInterval(i1)
+          clearInterval(i2)
+        }
+      }, Math.floor((Math.random()*200)+100))
     }
   }
-
-  var game = new Game()
+  
+  var Player = function(name, html){
+    var self = this
+    self.position = 0
+    self.name = name
+    self.hero = html
+    
+    self.doStep = function(){
+      self.position = self.position + 1
+      self.hero.toggleClass("alt")
+      self.hero.css("left", "" + self.position + "%")
+      if (self.position >= 80) {
+        alert(name + " win!")
+        document.location = document.location
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+  
+  var player1 = new Player("Ear", $(".player-1 .hero"))
+  var player2 = new Player("Van", $(".player-2 .hero"))
+  var game = new Game(player1, player2)
 
   $(window).one("keypress", function(e){
     if (e.which == 32) {
